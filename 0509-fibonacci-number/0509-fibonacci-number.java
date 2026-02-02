a0cafb028b85d4fb02948f1c39e92bd3c0a99600
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-02-02 20:24:31 +0530

    Time: 0 ms (100%), Space: 42.3 MB (12.19%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,14 +1,12 @@
 class Solution {
-    public int calFib(int n, int[] dp) {
-        if(n==1 || n==0) return n;
-        if(dp[n]!=0) return dp[n];
-        // dp[n] = calFib(n-1,dp) + calFib(n-2,dp);
-        // return dp[n];
-        return dp[n] = calFib(n-1,dp) + calFib(n-2,dp);
-    }
-
     public int fib(int n) {
+        if(n<=1) return n;
         int dp[] = new int[n+1];
-        return calFib(n,dp);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
