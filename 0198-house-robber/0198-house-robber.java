1ecd0ace4041170e9c567448c830138bfaaf1db1
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-02-03 17:07:50 +0530

    Time: 0 ms (100%), Space: 42.9 MB (30.74%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int find(int[]nums,int i,int dp[]) {
+    public int amount(int[] nums,int i,int[] dp) {
         int n = nums.length;
-        if(i==0)return nums[i];
-        if(i<0) return 0;
+        if(i>=n) return 0;
         if(dp[i]!=-1) return dp[i];
-        int pick = nums[i] + find(nums,i-2,dp);
-        int notpick = 0 + find(nums,i-1,dp);
-        return dp[i] = Math.max(pick,notpick);
+        int take = nums[i] + amount(nums,i+2,dp);
+        int skip = amount(nums,i+1,dp);
+        return dp[i] = Math.max(take,skip);
     }
     public int rob(int[] nums) {
-       int n = nums.length;
-       int dp[] = new int[n+1];
-       Arrays.fill(dp,-1);
-       return find(nums,n-1,dp);
+        // i varies from 0 to n-1
+        int n = nums.length;
+        int dp[] = new int[n];
+        Arrays.fill(dp,-1);
+        return amount(nums,0,dp);
     }
 }
