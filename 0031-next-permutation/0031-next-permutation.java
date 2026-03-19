b053db914248faba152ad2c92cf060c75d661f11
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-03-19 15:13:45 +0530

    Time: 1 ms (10.75%), Space: 44.6 MB (80.83%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        // find the pivot ele
+        int n = nums.length;
+        int pivot = -1;
+        for(int i=n-2;i>=0;i--) {
+            if(nums[i]<nums[i+1]) {
+                pivot = i;
+                break;
+            }
+        }
+        if(pivot==-1) {
+            Arrays.sort(nums);
+            return;
+        } 
+
+        // find the rightmost ele > pivot
+        for(int i=n-1;i>pivot;i--) {
+            if(nums[i]>nums[pivot]) {
+                // swap them 
+                int temp = nums[i];
+                nums[i] = nums[pivot];
+                nums[pivot] = temp;
+                break;
+            }
+        }
+        // now reverse the pivot+1 to n-1
+        int i=pivot+1;
+        int j=n-1;
+        while(i<=j) {
+            int temp = nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+        }
+    }
+}
