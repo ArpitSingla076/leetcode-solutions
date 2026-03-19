c66de531279a09115f82405c897c209b99320b93
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-03-19 15:21:57 +0530

    Time: 2 ms (8.49%), Space: 44.7 MB (64.35%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -18,9 +18,10 @@ class Solution {
         for(int i=n-1;i>pivot;i--) {
             if(nums[i]>nums[pivot]) {
                 // swap them 
-                int temp = nums[i];
-                nums[i] = nums[pivot];
-                nums[pivot] = temp;
+                // int temp = nums[i];
+                // nums[i] = nums[pivot];
+                // nums[pivot] = temp;
+                swap(nums,i,pivot);
                 break;
             }
         }
@@ -28,11 +29,18 @@ class Solution {
         int i=pivot+1;
         int j=n-1;
         while(i<=j) {
-            int temp = nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
+            // int temp = nums[i];
+            // nums[i]=nums[j];
+            // nums[j]=temp;
+            swap(nums,i,j);
             i++;
             j--;
         }
+    }
+
+    public void swap(int[]nums,int a,int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
