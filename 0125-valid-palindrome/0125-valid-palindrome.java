b1854112f5997a38aa8a89103b01baaca1470e85
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-03-26 13:14:24 +0530

    Time: 4 ms (49.28%), Space: 45.1 MB (41.69%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalin(String a) {
+        StringBuilder sb = new StringBuilder(a);
+        sb.reverse();
+        String res = sb.toString();
+        if(res.equals(a)) {
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+        for(int i=0;i<n;i++) {
+            char ch = s.charAt(i);
+            if(( ch>='a' && ch<='z') || (ch>='0' && ch<='9')) {
+                sb.append(ch);
+            }
+        }
+        String res = sb.toString();
+        if(isPalin(res)) return true;
+        return false;
+    }
+}
