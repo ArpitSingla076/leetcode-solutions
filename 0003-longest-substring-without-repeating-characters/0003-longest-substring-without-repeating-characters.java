c584c0ec679e3e5d1de4a2030974cd75686dc8b4
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-03-30 15:03:26 +0530

    Time: 6 ms (69.53%), Space: 46.5 MB (34.33%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,17 +1,38 @@
 // -- Brute force(generating all substring) --
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int n = s.length();
+//         int maxlen = 0;
+//         for(int i=0;i<n;i++) {
+//             HashSet<Character> set = new HashSet<>();
+//             for(int j=i;j<n;j++) {
+//                 char ch = s.charAt(j);
+//                 if(set.contains(ch)) break;
+//                 set.add(ch);
+//                 maxlen = Math.max(maxlen,j-i+1);
+//             }
+//         }
+//         return maxlen;
+//     }
+// }
+
+
+// -- Optima Sol (using two pointer and sliding window(HashSet)) --
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        int maxlen = 0;
-        for(int i=0;i<n;i++) {
-            HashSet<Character> set = new HashSet<>();
-            for(int j=i;j<n;j++) {
-                char ch = s.charAt(j);
-                if(set.contains(ch)) break;
-                set.add(ch);
-                maxlen = Math.max(maxlen,j-i+1);
-            }
+      int n = s.length();
+      int left = 0;
+      int maxlen = 0;
+      HashSet<Character> set = new HashSet<>();
+      for(int right=0;right<n;right++) {
+        char ch = s.charAt(right);
+        while(set.contains(ch)) {
+            set.remove(s.charAt(left));
+            left++;
         }
-        return maxlen;
+        set.add(ch);
+        maxlen = Math.max(maxlen,right-left+1);
+      }
+      return maxlen;
     }
 }
