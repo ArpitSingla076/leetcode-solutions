78e3a48dab89dfd84bc2937b6e08178d4501b37e
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-03-31 17:30:03 +0530

    Time: 3 ms (97.67%), Space: 52 MB (82.13%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,49 +1,49 @@
 // --- Brute Force (Nested Loop) ---
-class Solution {
-    public int longestOnes(int[] nums, int k) {
-        int n = nums.length;
-        int left = 0;
-        int maxlen = 0;
-        for(int i=0;i<n;i++) {
-            int zeros = 0;
-            for(int j=i;j<n;j++) {
-                if(nums[j]==0) {
-                    zeros++;
-                }
-                if(zeros<=k) {
-                    maxlen = Math.max(maxlen,j-i+1);
-                }
-                else{
-                    break;
-                }
-            }
-        }
-        return maxlen;
-    }
-}
-
-
-// --- Optimal App(using sliding window) ---
 // class Solution {
 //     public int longestOnes(int[] nums, int k) {
 //         int n = nums.length;
 //         int left = 0;
-//         int zeroCount = 0;
 //         int maxlen = 0;
-//         for(int right=0;right<n;right++) {
-//             if(nums[right]==0) {
-//                 zeroCount++;
-//             }
-
-//             // if zeros exceed K then shrink
-//             while(zeroCount>k) {
-//                 if(nums[left]==0) {
-//                     zeroCount--;
+//         for(int i=0;i<n;i++) {
+//             int zeros = 0;
+//             for(int j=i;j<n;j++) {
+//                 if(nums[j]==0) {
+//                     zeros++;
 //                 }
-//                 left++;
+//                 if(zeros<=k) {
+//                     maxlen = Math.max(maxlen,j-i+1);
+//                 }
+//                 else{
+//                     break;
+//                 }
 //             }
-//             maxlen = Math.max(maxlen,right-left+1);
 //         }
 //         return maxlen;
 //     }
 // }
+
+
+// --- Optimal App(using sliding window) ---
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int n = nums.length;
+        int left = 0;
+        int zeroCount = 0;
+        int maxlen = 0;
+        for(int right=0;right<n;right++) {
+            if(nums[right]==0) {
+                zeroCount++;
+            }
+
+            // if zeros exceed K then shrink
+            while(zeroCount>k) {
+                if(nums[left]==0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+            maxlen = Math.max(maxlen,right-left+1);
+        }
+        return maxlen;
+    }
+}
