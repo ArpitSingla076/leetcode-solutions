7cb65918cc67a401508e74b4f9c1883c266d60ea
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-05-07 15:01:46 +0530

    Time: 3 ms (87.59%), Space: 43.4 MB (40.66%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,22 +4,23 @@ class Solution {
         Stack<Character> st = new Stack<>();
         for(int i=0;i<n;i++) {
             char ch = s.charAt(i);
-            if(ch=='(' || ch== '{' || ch=='[') {
+            if(ch=='(' || ch=='[' || ch=='{') {
                 st.push(ch);
             }
             else{
                 if(st.size()==0) return false;
-                else {
-                    if(st.peek()=='(' && ch==')' || st.peek()=='[' && ch==']'
-                    || st.peek()=='{' && ch=='}') {
+                else{
+                    if(st.peek()=='(' && ch==')' || st.peek()=='{' && ch=='}' ||
+                    st.peek()=='[' && ch==']') {
                         st.pop();
                     }
-                    else return false;
-                   
+                    else{
+                        return false;
+                    }
                 }
             }
         }
         if(st.size()==0) return true;
         else return false;
     }
-}
+} 
