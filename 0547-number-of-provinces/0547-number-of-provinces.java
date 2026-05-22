fce175d4fa7d8ccbdd02da4286e89832e4a2035e
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-05-22 21:24:20 +0530

    Time: 1 ms (86.97%), Space: 47.1 MB (85.77%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,26 +1,38 @@
 class Solution {
-    public void bfs(int[][]adj,boolean[]vis,int i) {
+    // public void bfs(int[][]adj,boolean[]vis,int i) {
+    //     int n = adj.length;
+    //     vis[i] = true;
+    //     Queue<Integer> q = new LinkedList<>();
+    //     q.add(i);
+    //     while(q.size()>0) {
+    //         int curr = q.poll();
+    //         for(int j=0;j<n;j++) {
+    //             if(adj[curr][j]==1 && vis[j]==false) {
+    //                 q.add(j);
+    //                 vis[j] = true;
+    //             }
+    //         }
+    //     }
+    // }
+
+    public void dfs(int[][]adj,boolean[]vis,int i) {
         int n = adj.length;
         vis[i] = true;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(i);
-        while(q.size()>0) {
-            int curr = q.poll();
-            for(int j=0;j<n;j++) {
-                if(adj[curr][j]==1 && vis[j]==false) {
-                    q.add(j);
-                    vis[j] = true;
-                }
+        for(int j=0;j<n;j++) {
+            if(adj[i][j]==1 && vis[j]==false) {
+                dfs(adj,vis,j); 
             }
         }
     }
+
     public int findCircleNum(int[][] adj) {
         int n = adj.length;
         int count = 0;
         boolean[] vis = new boolean[n];
         for(int i=0;i<n;i++) {
             if(!vis[i]) {
-                bfs(adj,vis,i);
+               // bfs(adj,vis,i);
+               dfs(adj,vis,i);
                 count++;
             }
         }
