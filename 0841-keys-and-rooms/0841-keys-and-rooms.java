756a402e49c963dae707b68ed869ff42062ee865
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-05-23 15:23:09 +0530

    Time: 1 ms (85.81%), Space: 46.2 MB (63.55%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,22 +1,33 @@
 class Solution {
-    public void bfs(List<List<Integer>> adj,boolean[] vis,int i) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(i);
-        while(q.size()>0) {
-            int curr = q.poll();
-            for(int ele : adj.get(curr)) {
-                if(!vis[ele]) {
-                    q.add(ele);
-                    vis[ele] = true;
-                }
+    // public void bfs(List<List<Integer>> adj,boolean[] vis,int i) {
+    //     Queue<Integer> q = new LinkedList<>();
+    //     q.add(i);
+    //     while(q.size()>0) {
+    //         int curr = q.poll();
+    //         for(int ele : adj.get(curr)) {
+    //             if(!vis[ele]) {
+    //                 q.add(ele);
+    //                 vis[ele] = true;
+    //             }
+    //         }
+    //     }
+    // }
+
+    public void dfs(List<List<Integer>> adj,boolean[] vis,int i) {
+        vis[i] = true;
+        for(int neig : adj.get(i)) {
+            if(!vis[neig]) {
+                dfs(adj,vis,neig);
             }
         }
     }
+
     public boolean canVisitAllRooms(List<List<Integer>> adj) {
         int n = adj.size();
         boolean[] vis = new boolean[n];
         vis[0] = true;
-        bfs(adj,vis,0);
+       // bfs(adj,vis,0);
+        dfs(adj,vis,0);
         for(boolean ele : vis) {
             if(ele==false) return false;
         }
