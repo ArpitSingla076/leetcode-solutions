a4bd5a021f53c407e78456556ea4b6ebf8c3965f
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-05-27 16:13:39 +0530

    Time: 1 ms (10.51%), Space: 44.7 MB (49.73%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,46 +1,43 @@
 class Solution {
+    public void swap(int[]nums,int a,int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
     public void nextPermutation(int[] nums) {
-        // find the pivot ele
         int n = nums.length;
         int pivot = -1;
+
+        // find the pivot
         for(int i=n-2;i>=0;i--) {
             if(nums[i]<nums[i+1]) {
                 pivot = i;
                 break;
             }
         }
+
         if(pivot==-1) {
             Arrays.sort(nums);
             return;
         } 
 
-        // find the rightmost ele > pivot
+        // find rightmost ele > pivot
         for(int i=n-1;i>pivot;i--) {
             if(nums[i]>nums[pivot]) {
-                // swap them 
-                // int temp = nums[i];
-                // nums[i] = nums[pivot];
-                // nums[pivot] = temp;
+                // swap them
                 swap(nums,i,pivot);
                 break;
             }
         }
-        // now reverse the pivot+1 to n-1
+
+        // reverse pivot+1 to n-1
         int i=pivot+1;
         int j=n-1;
         while(i<=j) {
-            // int temp = nums[i];
-            // nums[i]=nums[j];
-            // nums[j]=temp;
             swap(nums,i,j);
             i++;
             j--;
         }
-    }
-
-    public void swap(int[]nums,int a,int b) {
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
     }
 }
