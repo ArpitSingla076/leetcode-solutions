b0e62bdd3b83a50e72e9996d42d9bb23748166fa
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-05-29 22:25:08 +0530

    Time: 3 ms (88.06%), Space: 43.1 MB (84.14%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,23 +4,21 @@ class Solution {
         Stack<Character> st = new Stack<>();
         for(int i=0;i<n;i++) {
             char ch = s.charAt(i);
-            if(ch=='(' || ch=='[' || ch=='{') {
+            if(ch=='(' || ch=='{' || ch=='[') {
                 st.push(ch);
             }
             else{
                 if(st.size()==0) return false;
+                else if( (st.peek()=='(' && ch==')') || (st.peek()=='[' && ch==']')
+                || (st.peek()=='{' && ch=='}')) {
+                    st.pop();
+                }
                 else{
-                    if(st.peek()=='(' && ch==')' || st.peek()=='{' && ch=='}' ||
-                    st.peek()=='[' && ch==']') {
-                        st.pop();
-                    }
-                    else{
-                        return false;
-                    }
+                    return false;
                 }
             }
         }
         if(st.size()==0) return true;
         else return false;
     }
-} 
+}
