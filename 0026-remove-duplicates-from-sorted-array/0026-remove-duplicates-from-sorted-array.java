3f58e5fb8b3af2c4f2209cdf73ac85511d0d0bbf
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-02 20:21:21 +0530

    Time: 1 ms (78.35%), Space: 46.7 MB (62.39%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,13 +1,27 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        // int n = nums.length;
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int val : nums) {
+        //     map.put(val,map.getOrDefault(val,0)+1);
+        // }
+        // int idx = 0;
+        // for(int key : map.keySet()) {
+        //     nums[idx++] = key;
+        // }
+        // return idx;
+
+    // --- Two Pointer App --
         int n = nums.length;
-        int idx=1;
-        for(int i=1;i<n;i++) {
-            if(nums[i]!=nums[i-1]) {
-                nums[idx] = nums[i];
-                idx++;
+        int i=0;
+        for(int j=1;j<n;j++) {
+            if(nums[i]<nums[j]) {
+                int temp = nums[i+1];
+                nums[i+1] = nums[j];
+                nums[j] = temp;
+                i++;
             }
         }
-        return idx;
+        return i+1;
     }
 }
