de044e13ef457caa93558b2e5be49fd76cdc27f7
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-03 11:57:22 +0530

    Time: 48 ms (13.28%), Space: 47.9 MB (36.09%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,18 +1,18 @@
-class Solution {
-    class Pair{
-        int key;
-        int val;
-        Pair(int key,int val) {
-            this.key = key;
-            this.val = val;
-        }
+class Pair{
+    int key;
+    int val;
+    Pair(int key,int val) {
+        this.key = key;
+        this.val = val;
     }
+}
 
+class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         List<Integer> ans = new ArrayList<>();
         PriorityQueue<Pair> maxheap = new PriorityQueue<>((a,b) -> {
-            if(a.key != b.key) return b.key - a.key;
-            return b.val - a.val;
+           if(a.key!=b.key) return b.key - a.key;
+            else return b.val - a.val;
         });
 
         int n = arr.length;
@@ -20,12 +20,12 @@ class Solution {
             int key = Math.abs(arr[i]-x);
             maxheap.add(new Pair(key,arr[i]));
             if(maxheap.size()>k) {
-                maxheap.remove();
+                maxheap.poll();
             }
         }
 
         while(maxheap.size()>0) {
-            ans.add(maxheap.remove().val);
+            ans.add(maxheap.poll().val);
         }
         Collections.sort(ans);
         return ans;
