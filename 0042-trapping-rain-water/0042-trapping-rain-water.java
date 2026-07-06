bcb5c75fe790821a5e0d183d5114f7b6c3029fc1
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-07 00:32:04 +0530

    Time: 1 ms (60.12%), Space: 47.3 MB (97.64%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,62 +1,23 @@
 class Solution {
     public int trap(int[] height) {
-        // -- Brute force (Using nested loops) --
-        // int n = height.length;
-        // int total = 0;
-
-        // for(int i=0;i<n;i++) {
-        //     int lmax = height[i];
-        //     for(int j=0;j<=i;j++) {
-        //         lmax = Math.max(lmax, height[j]);
-        //     }
-
-        //     int rmax = height[i];
-        //     for (int j=i;j<n;j++) {
-        //         rmax = Math.max(rmax, height[j]);
-        //     }
-        //     total += Math.min(lmax, rmax) - height[i];
-        // }
-        // return total;
-
-
-        // -- Optimal sol(using Prefix Array) --
-        // int n = height.length;
-        // int[] lmax = new int[n];
-        // int[] rmax = new int[n];
-        // int ans = 0;
-        // lmax[0] = height[0];
-        // rmax[n-1] = height[n-1];
-        // for(int i=1;i<n;i++) {
-        //     lmax[i] = Math.max(lmax[i-1],height[i]);
-        // }
-        // for(int i=n-2;i>=0;i--) {
-        //     rmax[i] = Math.max(rmax[i+1],height[i]);
-        // }
-        // for(int i=0;i<n;i++) {
-        //     ans+= Math.min(lmax[i],rmax[i]) - height[i];
-        // }
-        // return ans;
-
-
-        // ---Optimal Sol(using 2 Pointer) ---
         int n = height.length;
-        int left = 0;
-        int right = n-1;
+        int i=0;
+        int j=n-1;
         int lmax=0;
         int rmax=0;
-        int ans=0;
-        while(left<right) {
-            lmax = Math.max(lmax,height[left]);
-            rmax = Math.max(rmax,height[right]);
-            if(lmax<rmax) { // lmax will decide how much water will store
-                ans+=(lmax-height[left]);
-                left++;
+        int total=0;
+        while(i<j) {
+            lmax = Math.max(lmax,height[i]);
+            rmax = Math.max(rmax,height[j]);
+            if(lmax<rmax) {
+                total+=lmax-height[i];
+                i++;
             }
-            else{ // rmax will decide how much water will store
-                ans+=(rmax-height[right]);
-                right--;
+            else{
+                total+=rmax-height[j];
+                j--;
             }
         }
-        return ans;
+        return total;
     }
 }
