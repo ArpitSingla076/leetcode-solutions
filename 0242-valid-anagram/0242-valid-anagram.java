429b12e87f4f5d06af0a9c932ffc9aecd59ac29b
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-08 15:07:50 +0530

    Time: 6 ms (43.77%), Space: 44.2 MB (82.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -32,18 +32,29 @@
 // -- Optimal(using freq array) --
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
+        // if (s.length() != t.length()) return false;
 
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
+        // int[] freq1 = new int[26];
+        // int[] freq2 = new int[26];
 
-        for(int i=0; i<s.length();i++) {
-            freq1[s.charAt(i)-'a']++;
-            freq2[t.charAt(i)-'a']++;
+        // for(int i=0; i<s.length();i++) {
+        //     freq1[s.charAt(i)-'a']++;
+        //     freq2[t.charAt(i)-'a']++;
+        // }
+
+        // for(int i=0;i<26;i++) {
+        //     if(freq1[i]!=freq2[i]) return false;
+        // }
+        // return true;
+
+        if(s.length()!=t.length()) return false;
+        int[] freq = new int[26];
+        for(int i=0;i<s.length();i++) {
+            freq[s.charAt(i)-'a']++;
+            freq[t.charAt(i)-'a']--;
         }
-
-        for(int i=0;i<26;i++) {
-            if(freq1[i]!=freq2[i]) return false;
+        for(int val : freq) {
+            if(val!=0) return false;
         }
         return true;
     }
