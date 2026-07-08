0dc34ef9785054d13f8bc93197d545f63ca1fa01
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-08 22:53:43 +0530

    Time: 14 ms (70.75%), Space: 47.2 MB (97.47%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,36 +1,37 @@
-class Solution {
-    class Pair{
-        int freq;
-        int val;
-        Pair(int freq,int val) {
-            this.freq = freq;
-            this.val = val;
-        }
+class Pair{
+    int freq;
+    int val;
+    Pair(int freq,int val) {
+        this.freq = freq;
+        this.val = val;
     }
+}
 
+class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         int[] ans = new int[k];
-        PriorityQueue<Pair> minheap = new PriorityQueue<>((a,b) -> {
-            if(a.freq != b.freq) return a.freq - b.freq;
-            return a.val - b.val;
-        });
+        int n = nums.length;
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int val : nums) {
             map.put(val,map.getOrDefault(val,0)+1);
         }
 
-        for(int key:map.keySet()) {
+        PriorityQueue<Pair> minheap = new PriorityQueue<>((a,b) -> {
+            if(a.freq != b.freq) return a.freq-b.freq;
+            else return a.val - b.val;
+        });
+
+        for(int key : map.keySet()) {
             minheap.add(new Pair(map.get(key),key));
             if(minheap.size()>k) {
-                minheap.remove();
+                minheap.poll();
             }
         }
 
         int idx=0;
         while(minheap.size()>0) {
-            ans[idx++] = minheap.remove().val;
+            ans[idx++] = minheap.poll().val;
         }
         return ans;
-
     }
 }
