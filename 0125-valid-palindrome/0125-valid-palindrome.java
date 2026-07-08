98c90bd54480b90f6f72d6d37603bc8287ea8ed2
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-08 23:11:20 +0530

    Time: 4 ms (48.53%), Space: 45 MB (41.52%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,56 +1,28 @@
-// class Solution {
-//     public boolean isPalin(String a) {
-//         StringBuilder sb = new StringBuilder(a);
-//         // sb.reverse();
-//         // String res = sb.toString();
-//         // if(res.equals(a)) {
-//         //     return true;
-//         // }
-//         // return false;
-//         return sb.reverse().toString().equals(a);
-//     }
-    
-//     public boolean isPalindrome(String s) {
-//         s = s.toLowerCase();
-//         StringBuilder sb = new StringBuilder();
-//         int n = s.length();
-//         for(int i=0;i<n;i++) {
-//             char ch = s.charAt(i);
-//             if(( ch>='a' && ch<='z') || (ch>='0' && ch<='9')) {
-//                 sb.append(ch);
-//             }
-//         }
-//         String res = sb.toString();
-//         if(isPalin(res)) return true;
-//         return false;
-//     }
-// }
-
-
-// --- Using two pointer App ---
-
-class Solution{
-    public boolean isPalindrome(String s) {
-        int start = 0;
-        int end = s.length()-1;
-        while(start<end) {
-            char left = s.charAt(start);
-            char right = s.charAt(end);
-            if(!Character.isLetterOrDigit(left)) {
-                start++;
-                continue;
-            }
-
-            if(!Character.isLetterOrDigit(right)) {
-                end--;
-                continue;
-            }
-            if(Character.toLowerCase(left)!=Character.toLowerCase(right)) {
-                return false;
-            }
-            start++;
-            end--;
+class Solution {
+    public boolean isPalin(String a) {
+        int n = a.length();
+        int i=0;
+        int j=n-1;
+        while(i<j) {
+            if(a.charAt(i)!=a.charAt(j)) return false;
+            i++;
+            j--;
         }
         return true;
+    }
+
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        s = s.toLowerCase();
+        int n = s.length();
+        for(int i=0;i<n;i++) {
+            char ch = s.charAt(i);
+            if(ch>='a' && ch<='z' || ch>='0' && ch<='9') {
+                sb.append(ch);
+            }
+        }
+        String ans = sb.toString();
+        if(isPalin(ans)) return true;
+        else return false;
     }
 }
