d844889528d6e43938f9a0051df00e019932c961
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-09 14:27:32 +0530

    Time: 6 ms (70.24%), Space: 46.4 MB (38.5%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,58 +1,35 @@
-// -- Brute force(generating all substring) --
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         int n = s.length();
-//         int maxlen = 0;
-//         for(int i=0;i<n;i++) {
-//             HashSet<Character> set = new HashSet<>();
-//             for(int j=i;j<n;j++) {
-//                 char ch = s.charAt(j);
-//                 if(set.contains(ch)) break;
-//                 set.add(ch);
-//                 maxlen = Math.max(maxlen,j-i+1);
-//             }
-//         }
-//         return maxlen;
-//     }
-// }
-
-
-// -- Optima Sol (using two pointer and sliding window(HashSet)) --
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//       int n = s.length();
-//       int left = 0;
-//       int maxlen = 0;
-//       HashSet<Character> set = new HashSet<>();
-//       for(int right=0;right<n;right++) {
-//         char ch = s.charAt(right);
-//         while(set.contains(ch)) {
-//             set.remove(s.charAt(left));
-//             left++;
-//         }
-//         set.add(ch);
-//         maxlen = Math.max(maxlen,right-left+1);
-//       }
-//       return maxlen;
-//     }
-// }
-
-
-// -- Optima Sol (using two pointer and sliding window(HashMap)) --
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-      int n = s.length();
-      int left = 0;
-      int maxlen = 0;
-      HashMap<Character,Integer> map = new HashMap<>();
-      for(int right=0;right<n;right++) {
-        char ch = s.charAt(right);
-        if(map.containsKey(ch)) {
-            left = Math.max(left,map.get(ch)+1);
+        // --------- Brute force(genrate all substring) ----------
+        // int n = s.length();
+        // int maxlen = 0;
+        // for(int i=0;i<n;i++) {
+        //     HashSet<Character>set = new HashSet<>();
+        //     for(int j=i;j<n;j++) {
+        //         char ch = s.charAt(j);
+        //         if(set.contains(ch)) break;
+        //         set.add(ch);
+        //         maxlen = Math.max(maxlen,j-i+1);
+        //     }
+        // }
+        // return maxlen;
+
+
+        // ---- two pointer & sliding window(using Hashset) ----
+        int n = s.length();
+        int l=0;
+        int r=0;
+        int maxlen=0;
+        HashSet<Character> set = new HashSet<>();
+        for(r=0;r<n;r++) {
+            char ch = s.charAt(r);
+            while(set.contains(ch)) {
+                set.remove(s.charAt(l));
+                l++;
+            }
+            set.add(ch);
+            maxlen = Math.max(maxlen,r-l+1);
         }
-        map.put(ch,right);
-        maxlen = Math.max(maxlen,right-left+1);
-      }
-      return maxlen;
+        return maxlen;
     }
 }
