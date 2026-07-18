1b7c831dbc35dd6f99bc0f541041402787219046
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-18 14:52:17 +0530

    Time: 4 ms (5.49%), Space: 46.3 MB (97.81%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,27 +1,27 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // int n = nums.length;
-        // HashMap<Integer,Integer> map = new HashMap<>();
-        // for(int val : nums) {
-        //     map.put(val,map.getOrDefault(val,0)+1);
-        // }
-        // int idx = 0;
-        // for(int key : map.keySet()) {
-        //     nums[idx++] = key;
-        // }
-        // return idx;
+        int n = nums.length;
+        LinkedHashMap<Integer,Integer> map = new LinkedHashMap<>();
+        for(int val : nums) {
+            map.put(val,map.getOrDefault(val,0)+1);
+        }
+        int idx = 0;
+        for(int key : map.keySet()) {
+            nums[idx++] = key;
+        }
+        return idx;
 
     // --- Two Pointer App --
-        int n = nums.length;
-        int i=0;
-        for(int j=1;j<n;j++) {
-            if(nums[i]<nums[j]) {
-                int temp = nums[i+1];
-                nums[i+1] = nums[j];
-                nums[j] = temp;
-                i++;
-            }
-        }
-        return i+1;
+        // int n = nums.length;
+        // int i=0;
+        // for(int j=1;j<n;j++) {
+        //     if(nums[i]<nums[j]) {
+        //         int temp = nums[i+1];
+        //         nums[i+1] = nums[j];
+        //         nums[j] = temp;
+        //         i++;
+        //     }
+        // }
+        // return i+1;
     }
 }
