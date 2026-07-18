a2bafe066c97372a489ec65725db35983a03e0ad
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-18 14:59:03 +0530

    Time: 1 ms (78.76%), Space: 46.4 MB (92.32%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,27 +1,41 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int n = nums.length;
-        LinkedHashMap<Integer,Integer> map = new LinkedHashMap<>();
-        for(int val : nums) {
-            map.put(val,map.getOrDefault(val,0)+1);
-        }
-        int idx = 0;
-        for(int key : map.keySet()) {
-            nums[idx++] = key;
-        }
-        return idx;
+        // -------- Using Map ----------------
+        // int n = nums.length;
+        // LinkedHashMap<Integer,Integer> map = new LinkedHashMap<>();
+        // for(int val : nums) {
+        //     map.put(val,map.getOrDefault(val,0)+1);
+        // }
+        // int idx = 0;
+        // for(int key : map.keySet()) {
+        //     nums[idx++] = key;
+        // }
+        // return idx;
+
+        // -------- using Set --------------
+        // int n = nums.length;
+        // LinkedHashSet<Integer> set = new LinkedHashSet<>();
+        // for(int val : nums) {
+        //     set.add(val);
+        // }
+        // int idx=0;
+        // for(int val : set) {
+        //     nums[idx++] = val;
+        // }
+        // return set.size();
+
 
     // --- Two Pointer App --
-        // int n = nums.length;
-        // int i=0;
-        // for(int j=1;j<n;j++) {
-        //     if(nums[i]<nums[j]) {
-        //         int temp = nums[i+1];
-        //         nums[i+1] = nums[j];
-        //         nums[j] = temp;
-        //         i++;
-        //     }
-        // }
-        // return i+1;
+        int n = nums.length;
+        int i=0;
+        for(int j=1;j<n;j++) {
+            if(nums[i]<nums[j]) {
+                int temp = nums[i+1];
+                nums[i+1] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+        }
+        return i+1;
     }
 }
