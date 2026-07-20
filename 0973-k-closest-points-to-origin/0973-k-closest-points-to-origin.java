3c4547611d2cfc7483e4ae9737fec45fd023cbbe
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-21 00:48:51 +0530

    Time: 30 ms (66.87%), Space: 59.3 MB (15.36%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -2,7 +2,7 @@ class Pair{
     int dist;
     int x;
     int y;
-    Pair(int dist,int x,int y) {
+    public Pair(int dist,int x,int y) {
         this.dist = dist;
         this.x = x;
         this.y = y;
@@ -11,9 +11,10 @@ class Pair{
 
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        int[][] ans = new int[k][2];
-        PriorityQueue<Pair>maxheap = new PriorityQueue<>((a,b) -> b.dist-a.dist);
-        for(int i=0;i<points.length;i++) {
+        int n = points.length;
+        int[][] res = new int[k][2];
+        PriorityQueue<Pair> maxheap = new PriorityQueue<>((a,b)->b.dist - a.dist);
+        for(int i=0;i<n;i++) {
             int x = points[i][0];
             int y = points[i][1];
             int dist = x*x + y*y;
@@ -22,16 +23,15 @@ class Solution {
                 maxheap.poll();
             }
         }
-
         int idx=0;
         while(maxheap.size()>0) {
             Pair p = maxheap.poll();
             int x = p.x;
             int y = p.y;
-            ans[idx][0] = x;
-            ans[idx][1] = y;
+            res[idx][0] = x;
+            res[idx][1] = y;
             idx++;
         }
-        return ans;
+        return res;
     }
 }
