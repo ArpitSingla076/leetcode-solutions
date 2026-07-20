3b43d8818ff712fa577787f2e67def5cb88327ee
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-20 14:25:13 +0530

    Time: 31 ms (60.34%), Space: 95.6 MB (51.78%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,18 +1,40 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        int n = nums.length;
-        if(n==0) return 0;
-        Arrays.sort(nums);
-        int len = 1;
-        int maxlen=1;
-        for(int i=0;i<n-1;i++) {
-            if(nums[i]+1==nums[i+1]) {
-                len++;
-                maxlen = Math.max(len,maxlen);
-            }
-            else if(nums[i]==nums[i+1]) continue;
-            else if(nums[i]<nums[i+1]) {
-                len = 1;
+        // int n = nums.length;
+        // if(n==0) return 0;
+        // Arrays.sort(nums);
+        // int len = 1;
+        // int maxlen=1;
+        // for(int i=0;i<n-1;i++) {
+        //     if(nums[i]+1==nums[i+1]) {
+        //         len++;
+        //         maxlen = Math.max(len,maxlen);
+        //     }
+        //     else if(nums[i]==nums[i+1]) continue;
+        //     else {
+        //         len = 1;
+        //     }
+        // }
+        // return maxlen;
+
+
+        // -- using set --
+        HashSet<Integer>set = new HashSet<>();
+        for(int num : nums) {
+            set.add(num);
+        }
+        int maxlen=0;
+        for(int num : set) {
+            // Start only if num is the beginning of a sequence
+            if(!set.contains(num-1)) {
+                int curr = num;
+                int len=1;
+
+                while(set.contains(curr+1)) {
+                    curr++;
+                    len++;
+                }
+                maxlen = Math.max(maxlen,len);
             }
         }
         return maxlen;
