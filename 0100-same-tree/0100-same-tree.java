6fefe432480e31c8ecd19b51846734d7c9a9539b
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-21 17:28:41 +0530

    Time: 0 ms (100%), Space: 43.1 MB (15.33%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -18,8 +18,7 @@ class Solution {
         if(p==null && q==null) return true;
         if(p==null || q==null) return false;
         if(p.val!=q.val) return false;
-
-        return (p.val == q.val) &&
-     isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        return (p.val==q.val) && 
+        isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }
