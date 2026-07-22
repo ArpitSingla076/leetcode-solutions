44973ab7befdf766b408135a97820b1c4c09a275
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-22 14:41:24 +0530

    Time: 1 ms (99.89%), Space: 55.9 MB (6.6%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,16 +1,44 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<n;i++) {
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
+        // -- Brute foce --
+        // int n = nums.length;
+        // for(int i=0;i<n;i++) {
+        //     int cnt=0;
+        //     for(int j=0;j<n;j++) {
+        //         if(nums[j]==nums[i]) cnt++;
+        //     }
+        //     if(cnt>n/2) return nums[i];
+        // }
+        // return -1;
 
-        for(int key : map.keySet()) {
-            if(map.get(key)>n/2) {
-                return key;
-            }
+
+        // -- Better App(using map) --
+        // int n = nums.length;
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int i=0;i<n;i++) {
+        //     map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        // }
+
+        // for(int key : map.keySet()) {
+        //     if(map.get(key)>n/2) {
+        //         return key;
+        //     }
+        // }
+        // return 0;
+
+
+        // -- Moore's Voting Algo --
+        int n = nums.length;
+        int ans=0;
+        int cnt=0;
+        for(int i=0;i<n;i++) {
+            if(cnt==0) {
+                ans = nums[i];
+                cnt=1;
+            } 
+            else if(ans==nums[i]) cnt++;
+            else cnt--;
         }
-        return 0;
+        return ans;
     }
 }
