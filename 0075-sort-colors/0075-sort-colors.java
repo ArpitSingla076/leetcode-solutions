36eb7f7ffc6984a00537037d83a18cd07500dec3
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-22 14:22:57 +0530

    Time: 0 ms (100%), Space: 43.4 MB (61.14%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,31 +1,48 @@
 class Solution {
+    public void swap(int[]nums,int a,int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
     public void sortColors(int[] nums) {
+        // -- Brute force --
+        // int n = nums.length;
+        // int cnt0=0;
+        // int cnt1=0;
+        // int cnt2=0;
+        // for(int val : nums) {
+        //     if(val==0) cnt0++;
+        //     if(val==1) cnt1++;
+        //     if(val==2) cnt2++;
+        // }
+        // int idx=0;
+        // for(int i=0;i<cnt0;i++) {
+        //     nums[idx++] = 0;
+        // }
+        // for(int i=0;i<cnt1;i++) {
+        //     nums[idx++] = 1;
+        // }
+        // for(int i=0;i<cnt2;i++) {
+        //     nums[idx++] = 2;
+        // }
+
+
+        // -- Dutch Flag Algo --
         int n = nums.length;
-        int count0=0;
-        int count1=0;
-        int count2=0;
-
-        for(int i=0;i<n;i++) {
-            if(nums[i]==0) {
-                count0++;
+        int low = 0; 
+        int mid = 0;
+        int high = n-1;
+        while(mid<=high) {
+            if(nums[mid]==0) {
+                swap(nums,mid,low);
+                low++;
+                mid++;
             }
-            else if(nums[i]==1) {
-                count1++;
-            }
+            else if(nums[mid]==1)mid++;
             else{
-                count2++;
+                swap(nums,mid,high);
+                high--;
             }
-        }
-
-        int idx=0;
-        for(int i=0;i<count0;i++) {
-            nums[idx++] = 0;
-        }
-        for(int i=0;i<count1;i++) {
-            nums[idx++] = 1;
-        }
-        for(int i=0;i<count2;i++) {
-            nums[idx++] = 2;
         }
     }
 }
