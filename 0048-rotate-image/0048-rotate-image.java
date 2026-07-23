90b53741bdc7976c1cb2f721261c62b3e63cd501
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-23 15:01:03 +0530

    Time: 0 ms (100%), Space: 43.8 MB (53.32%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,31 +1,52 @@
 class Solution {
-    public void swap(int[][]matrix,int i,int j) {
-        int temp = matrix[i][j];
-        matrix[i][j] = matrix[j][i];
-        matrix[j][i] = temp;
-    }
+    // public void swap(int[][]matrix,int i,int j){
+    //     int temp = matrix[i][j];
+    //     matrix[i][j] = matrix[j][i];
+    //     matrix[j][i] = temp;
+    // }
+
     public void rotate(int[][] matrix) {
+        // -- brute force --
+        // int n = matrix.length;
+        // int[][] ans = new int[n][n];
+        // for(int i=0;i<n;i++) {
+        //     for(int j=0;j<n;j++) {
+        //         ans[j][n-1-i]=matrix[i][j];
+        //     }
+        // }
+
+        // // copy back to matrix
+        // for(int i=0;i<n;i++) {
+        //     for(int j=0;j<n;j++) {
+        //         matrix[i][j] = ans[i][j];
+        //     }
+        // }
+
+
+        // -- optimal App (transpose+reverse each row) --
         int n = matrix.length;
 
         // transpose
-        for(int i=0;i<n-1;i++) {
-            for(int j=i+1;j<n;j++) {
-                swap(matrix,i,j);
+        for(int i=0;i<=n-2;i++) {
+            for(int j=i+1;j<=n-1;j++) {
+               // swap(matrix,i,j);
+               int temp = matrix[i][j];
+               matrix[i][j] = matrix[j][i];
+               matrix[j][i] = temp;
             }
         }
 
         // reverse each row
         for(int i=0;i<n;i++) {
-            int start=0,end = n-1;
-            while(start<=end) {
-                int temp = matrix[i][start];
-                matrix[i][start] = matrix[i][end];
+            int st=0;
+            int end=n-1;
+            while(st<=end) {
+                int temp = matrix[i][st];
+                matrix[i][st] = matrix[i][end];
                 matrix[i][end] = temp;
-                start++;
+                st++;
                 end--;
             }
         }
     }
 }
-
-
