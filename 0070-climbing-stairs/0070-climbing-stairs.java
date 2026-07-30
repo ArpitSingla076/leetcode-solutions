8a6cb2f4576737f30bb2f25eef003e5eb3441d0d
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-30 11:45:09 +0530

    Time: 0 ms (100%), Space: 42 MB (73.23%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,11 +1,26 @@
 class Solution {
+    // public int countways(int n,int[] dp) {
+    //     if(n==0 || n==1) return 1;
+    //     if(dp[n]!=-1) return dp[n];
+    //     dp[n] = countways(n-1,dp) + countways(n-2,dp);
+    //     return dp[n];
+    // }
     public int climbStairs(int n) {
-        int dp[] = new int[n+1];
+        // -- recursion -- 
+        // if(n==0 || n==1) return 1;
+        // return climbStairs(n-1) + climbStairs(n-2);
+
+        // -- memoization --
+        // int[] dp = new int[n+1];
+        // Arrays.fill(dp,-1);
+        // return countways(n,dp);
+
+        // -- tabulation --
+        int[] dp = new int[n+1];
         dp[0] = 1;
         dp[1] = 1;
-
         for(int i=2;i<=n;i++) {
-            dp[i] = dp[i-1] + dp[i-2];  
+            dp[i] = dp[i-1] + dp[i-2];
         }
         return dp[n];
     }
