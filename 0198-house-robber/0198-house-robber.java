807e9c5bf6eda195934e4f09e8abc80416bfcc16
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-30 12:54:44 +0530

    Time: 0 ms (100%), Space: 43.1 MB (5.5%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -14,18 +14,34 @@
 
 
 // -- this is memoization --
-class Solution {
-    public int maxamt(int i,int[] nums,int[] dp) {
-        if(i==0) return nums[0];
-        if(i==1) return Math.max(nums[0],nums[1]);
-        if(dp[i]!=-1) return dp[i];
-        return dp[i] =  Math.max(nums[i] + maxamt(i-2,nums,dp) , maxamt(i-1,nums,dp)); 
-    }
+// class Solution {
+//     public int maxamt(int i,int[] nums,int[] dp) {
+//         if(i==0) return nums[0];
+//         if(i==1) return Math.max(nums[0],nums[1]);
+//         if(dp[i]!=-1) return dp[i];
+//         return dp[i] =  Math.max(nums[i] + maxamt(i-2,nums,dp) , maxamt(i-1,nums,dp)); 
+//     }
 
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         int[] dp = new int[n];
+//         Arrays.fill(dp,-1);
+//         return maxamt(n-1,nums,dp);
+//     }
+// }
+
+
+// -- this is tabulation --
+class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
-        Arrays.fill(dp,-1);
-        return maxamt(n-1,nums,dp);
+        if(n==1) return nums[0];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++) {
+            dp[i] = Math.max(nums[i] + dp[i-2] , dp[i-1]);
+        }
+        return dp[n-1];
     }
 }
