6c3b202bea9c8633aceeca2e76fbd3aa27b7f2f4
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-31 19:52:36 +0530

    Time: 5 ms (67.02%), Space: 47.3 MB (35.44%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,21 +1,20 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int m = word1.length();
-        int n = word2.length();
-
-        int[][] dp = new int[m+1][n+1];
+        int n = word1.length();
+        int m = word2.length();
+        int[][] dp = new int[n+1][m+1];
 
         // initilisation
-        for(int i=0;i<m+1;i++) {
+        for(int i=0;i<n+1;i++) {
             dp[i][0] = i;
         }
-        for(int j=0;j<n+1;j++) {
+        for(int j=0;j<m+1;j++) {
             dp[0][j] = j;
         }
 
         // kaam
-        for(int i=1;i<m+1;i++) {
-            for(int j=1;j<n+1;j++) {
+        for(int i=1;i<n+1;i++) {
+            for(int j=1;j<m+1;j++) {
                 if(word1.charAt(i-1)==word2.charAt(j-1)) {
                     dp[i][j] = dp[i-1][j-1];
                 }
@@ -27,6 +26,6 @@ class Solution {
                 }
             }
         }
-        return dp[m][n];
+        return dp[n][m];
     }
 }
