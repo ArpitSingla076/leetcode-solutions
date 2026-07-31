f6dd9498f224d069cc343e3faa94d62039fb548f
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-31 22:55:00 +0530

    Time: 27 ms (47.17%), Space: 47.7 MB (18.58%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int lcs(String s,String rev) {
+        int n = s.length();
+        int m = rev.length();
+        int[][] dp = new int[n+1][m+1];
+
+        // initilisation
+        for(int i=0;i<n+1;i++) {
+            dp[i][0] = 0;
+        }
+        for(int j=0;j<m+1;j++) {
+            dp[0][j] = 0;
+        }
+
+        // kaam
+        for(int i=1;i<n+1;i++) {
+            for(int j=1;j<m+1;j++) {
+                if(s.charAt(i-1)==rev.charAt(j-1)) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    public int minInsertions(String s) {
+        int n = s.length();
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+        String rev = sb.toString();
+        int l = lcs(s,rev);
+        return n-l;
+    }
+}
