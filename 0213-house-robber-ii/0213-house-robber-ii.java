f07964dde6a0c254fc79383492d24153e72666c7
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-31 12:11:55 +0530

    Time: 0 ms (100%), Space: 42.9 MB (23.54%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,26 +1,28 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
+        if(n==0) return 0;
         if(n==1) return nums[0];
-        if(n==2) return Math.max(nums[0], nums[1]);
+        if(n==2) return Math.max(nums[0],nums[1]);
 
-        // case 1 : 0 to n-2 (ignore last house)
-        int dp1[] = new int[n];
+        // first i will rob 0 to n-2
+        int[] dp1 = new int[n];
         dp1[0] = nums[0];
         dp1[1] = Math.max(nums[0],nums[1]);
         for(int i=2;i<=n-2;i++) {
-            dp1[i] = Math.max(nums[i] + dp1[i-2],dp1[i-1]);
+            dp1[i] = Math.max(nums[i]+dp1[i-2],dp1[i-1]);
         }
-        int case1 = dp1[n-2];
+        int ans1 = dp1[n-2];
 
-        // case 2 : 1 to n-1 (ignore first house)
-        int dp2[] = new int[n];
+        // now rob from 1 to n-1
+        int[]dp2 = new int[n];
         dp2[1] = nums[1];
         dp2[2] = Math.max(nums[1],nums[2]);
         for(int i=3;i<=n-1;i++) {
-            dp2[i] = Math.max(nums[i] + dp2[i-2],dp2[i-1]);
+            dp2[i] = Math.max(nums[i]+dp2[i-2],dp2[i-1]);
         }
-        int case2 = dp2[n-1];
-        return Math.max(case1,case2);
+        int ans2 = dp2[n-1];
+
+        return Math.max(ans1,ans2);
     }
 }
