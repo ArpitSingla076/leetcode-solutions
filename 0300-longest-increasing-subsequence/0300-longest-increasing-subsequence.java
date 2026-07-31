436dc441a6b273b152f267a06ced87a19b9673d3
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-31 22:45:26 +0530

    Time: 75 ms (25.35%), Space: 169.4 MB (22.12%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,11 +1,20 @@
 class Solution {
-    public int lcs(int[]nums,int[]nums2) {
-        int m = nums.length;
-        int n = nums2.length;
-        int[][] dp = new int[m+1][n+1];
+     public int lcs(int[]nums,int[]nums2) {
+        int n = nums.length;
+        int m = nums2.length;
+        int[][] dp = new int[n+1][m+1];
 
-        for(int i=1;i<m+1;i++) {
-            for(int j=1;j<n+1;j++) {
+        // initilisation
+        for(int i=0;i<n+1;i++) {
+            dp[i][0] = 0;
+        }
+        for(int j=0;j<m+1;j++) {
+            dp[0][j] = 0;
+        }
+
+        // kaam
+        for(int i=1;i<n+1;i++) {
+            for(int j=1;j<m+1;j++) {
                 if(nums[i-1]==nums2[j-1]) {
                     dp[i][j] = 1 + dp[i-1][j-1];
                 }
@@ -14,7 +23,7 @@ class Solution {
                 }
             }
         }
-        return dp[m][n];
+        return dp[n][m];
     }
 
     public int lengthOfLIS(int[] nums) {
@@ -23,8 +32,8 @@ class Solution {
         for(int val : nums) {
             set.add(val);
         }
-        int[] nums2 = new int[set.size()];
         int idx=0;
+        int[] nums2 = new int[set.size()];
         for(int val : set) {
             nums2[idx++] = val;
         }
