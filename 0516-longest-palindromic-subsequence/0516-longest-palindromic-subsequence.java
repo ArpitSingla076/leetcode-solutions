d5e1b9ea9173886512cf1c93f89220b6aea3ccb3
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-31 19:37:04 +0530

    Time: 64 ms (15.13%), Space: 63.3 MB (82.99%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,9 +1,8 @@
 class Solution {
-   public static int lcs(String s,String rev) {
+    public int lcs(String s,String rev) {
         int n = s.length();
         int m = rev.length();
-
-        int dp[][] = new int[n+1][m+1];
+        int[][] dp = new int[n+1][m+1];
 
         // initilisation
         for(int i=0;i<n+1;i++) {
@@ -13,16 +12,14 @@ class Solution {
             dp[0][j] = 0;
         }
 
-        // code
+        // kaam
         for(int i=1;i<n+1;i++) {
             for(int j=1;j<m+1;j++) {
-                if(s.charAt(i-1) == rev.charAt(j-1)) {
-                    dp[i][j] = 1+ dp[i-1][j-1];
+                if(s.charAt(i-1)==rev.charAt(j-1)) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 }
                 else{
-                    int ans1 = dp[i-1][j];
-                    int ans2 = dp[i][j-1];
-                    dp[i][j] = Math.max(ans1,ans2);
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
@@ -31,7 +28,8 @@ class Solution {
 
     public int longestPalindromeSubseq(String s) {
         StringBuilder sb = new StringBuilder(s);
-        String rev = sb.reverse().toString();
+        sb.reverse();
+        String rev = sb.toString();
         return lcs(s,rev);
     }
 }
