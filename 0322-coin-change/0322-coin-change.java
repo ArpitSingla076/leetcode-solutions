2ee0378b59cd58dc0176cfba8d7b76923bbd05f8
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-07-31 17:33:57 +0530

    Time: 15 ms (82%), Space: 47 MB (24.84%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,8 +2,8 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         int[][] dp = new int[n+1][amount+1];
-        int inf = (int)1e9;
 
+        int inf = (int)1e9;
         // initilisation
         for(int i=0;i<n+1;i++) {
             dp[i][0] = 0;
@@ -16,13 +16,13 @@ class Solution {
         for(int i=1;i<n+1;i++) {
             for(int j=1;j<amount+1;j++) {
                 if(coins[i-1]<=j) {
-                    dp[i][j] = Math.min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
+                    dp[i][j] = Math.min(1+ dp[i][j-coins[i-1]],dp[i-1][j]);
                 }
                 else{
                     dp[i][j] = dp[i-1][j];
                 }
             }
         }
-        return dp[n][amount]>=inf?-1:dp[n][amount];
+        return dp[n][amount]==inf?-1:dp[n][amount];
     }
 }
