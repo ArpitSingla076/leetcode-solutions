ffcd19c6bcd876e1d2a0027adc6cc187da0c4330
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-08-01 21:03:43 +0530

    Time: 142 ms (42.84%), Space: 287 MB (32.51%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,47 +1,33 @@
 class Solution {
-    // public void bfs(List<List<Integer>> adj,boolean[] vis,int i) {
-    //     vis[i] = true;
-    //     Queue<Integer> q = new LinkedList<>();
-    //     q.add(i);
-    //     while(q.size()>0) {
-    //         int curr = q.poll();
-    //         for(int neig : adj.get(curr)) {
-    //             if(!vis[neig]) {
-    //                 q.add(neig);
-    //                 vis[neig] = true;
-    //             }
-    //         }
-    //     }
-    // }
-
-    public void dfs(List<List<Integer>> adj,boolean[] vis,int i) {
-        vis[i] = true;
-        for(int neig : adj.get(i)) {
-            if(!vis[neig]) {
-                dfs(adj,vis,neig);
+    public void bfs(List<List<Integer>> adj,boolean[] vis,int src) {
+        int n = adj.size();
+        Queue<Integer>q = new LinkedList<>();
+        q.add(src);
+        vis[src] = true;
+        while(q.size()>0) {
+            int curr = q.poll();
+            for(int neig : adj.get(curr)) {
+                if(!vis[neig]) {
+                    q.add(neig);
+                    vis[neig] = true;
+                }
             }
         }
     }
-
-    public boolean validPath(int n, int[][] edges, int start, int end) {
-        if(start==end) return true;
+    public boolean validPath(int n, int[][] edges, int src, int dest) {
+        if(src==dest) return true;
         List<List<Integer>> adj = new ArrayList<>();
         for(int i=0;i<n;i++) {
-            // List<Integer> sub = new ArrayList<>();
-            // adj.add(sub);
             adj.add(new ArrayList<>());
         }
-
         for(int i=0;i<edges.length;i++) {
-            int x = edges[i][0];
-            int y = edges[i][1];
-            adj.get(x).add(y);
-            adj.get(y).add(x);
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
-
         boolean[] vis = new boolean[n];
-       // bfs(adj,vis,start);
-       dfs(adj,vis,start);
-        return vis[end];
+        bfs(adj,vis,src);
+        return vis[dest];
     }
 }
