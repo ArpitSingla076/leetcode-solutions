db4e310d228a2c8a8e5dfcb4bb7ce631cf905990
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-08-03 19:43:57 +0530

    Time: 4 ms (45.17%), Space: 52.5 MB (22.33%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,35 +1,74 @@
-class Pair{
-    int row;
-    int col;
-    Pair(int row,int col) {
-        this.row = row;
-        this.col = col;
-    }
-}
+// -- bfs App --
+// class Pair{
+//     int row;
+//     int col;
+//     Pair(int row,int col) {
+//         this.row = row;
+//         this.col = col;
+//     }
+// }
+// class Solution {
+//     public void bfs(char[][] grid,boolean[][] vis,int i,int j) {
+//         int m = grid.length;
+//         int n = grid[0].length;
+//         Queue<Pair> q = new LinkedList<>();
+//         q.add(new Pair(i,j));
+//         vis[i][j] = true;
+//         while(q.size()>0) {
+//             Pair curr = q.poll();
+//             int row = curr.row;
+//             int col = curr.col;
+//             int[] drow = {-1,0,1,0};
+//             int[] dcol = {0,1,0,-1};
+//             for(int k=0;k<4;k++) {
+//                 int nrow = row+drow[k];
+//                 int ncol = col+dcol[k];
+//                 if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol]=='1' && vis[nrow][ncol]==false) {
+//                     q.add(new Pair(nrow,ncol));
+//                     vis[nrow][ncol] = true;
+//                 }
+//             }
+
+//         }
+//     }
+//     public int numIslands(char[][] grid) {
+//         int m = grid.length;
+//         int n = grid[0].length;
+//         int count=0;
+//         boolean[][] vis = new boolean[m][n];
+//         for(int i=0;i<m;i++) {
+//             for(int j=0;j<n;j++) {
+//                 if(grid[i][j]=='1' && vis[i][j]==false) {
+//                     bfs(grid,vis,i,j);
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+
+// -- dfs App  --
 class Solution {
-    public void bfs(char[][] grid,boolean[][] vis,int i,int j) {
+    public void dfs(char[][] grid,boolean[][] vis,int i,int j) {
         int m = grid.length;
         int n = grid[0].length;
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(i,j));
-        vis[i][j] = true;
-        while(q.size()>0) {
-            Pair curr = q.poll();
-            int row = curr.row;
-            int col = curr.col;
-            int[] drow = {-1,0,1,0};
-            int[] dcol = {0,1,0,-1};
-            for(int k=0;k<4;k++) {
-                int nrow = row+drow[k];
-                int ncol = col+dcol[k];
-                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol]=='1' && vis[nrow][ncol]==false) {
-                    q.add(new Pair(nrow,ncol));
-                    vis[nrow][ncol] = true;
-                }
-            }
 
-        }
+        // boundary condn
+        if(i<0 || j<0 || i>=m || j>=n) return;
+
+        // water or already vis
+        if(grid[i][j]=='0' || vis[i][j]==true) return;
+
+        vis[i][j] = true;
+        // check neig
+        dfs(grid,vis,i-1,j);
+        dfs(grid,vis,i,j+1);
+        dfs(grid,vis,i+1,j);
+        dfs(grid,vis,i,j-1);
     }
+
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
@@ -38,11 +77,11 @@ class Solution {
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
                 if(grid[i][j]=='1' && vis[i][j]==false) {
-                    bfs(grid,vis,i,j);
+                    dfs(grid,vis,i,j);
                     count++;
                 }
             }
         }
-        return count;
+       return count;
     }
 }
