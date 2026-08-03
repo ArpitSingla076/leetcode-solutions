08b2fa7af7f8be22b01d7ccaf9fc658cbe344065
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-08-03 19:34:13 +0530

    Time: 1 ms (86.6%), Space: 47.3 MB (44.46%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,4 +1,14 @@
 class Solution {
+    // public void dfs(int[][]adj,boolean[]vis,int node) {
+    //     int n = adj.length;
+    //     vis[node] = true;
+    //     for(int j=0;j<n;j++) {
+    //         if(adj[node][j]==1 && vis[j]==false) {
+    //             dfs(adj,vis,j);
+    //         }
+    //     }
+    // }
+
     public void bfs(int[][]adj,boolean[] vis,int node) {
         int n = adj.length;
         Queue<Integer> q = new LinkedList<>();
@@ -13,8 +23,8 @@ class Solution {
                 }
             }
         }
-       
     }
+
     public int findCircleNum(int[][] adj) {
         int n = adj.length;
         int count=0;
@@ -22,6 +32,7 @@ class Solution {
         for(int i=0;i<n;i++) {
             if(!vis[i]) {
                 bfs(adj,vis,i); 
+             // dfs(adj,vis,i);
                 count++;
             }
         }
