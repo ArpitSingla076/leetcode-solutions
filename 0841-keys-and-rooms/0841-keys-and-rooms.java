4d667ee37e24d31fb706224a16ed01b821f3f9ab
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-08-03 20:42:58 +0530

    Time: 2 ms (45.21%), Space: 46.5 MB (11.67%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,4 +1,13 @@
 class Solution {
+    // public void dfs(List<List<Integer>> adj,boolean[] vis,int node) {
+    //     vis[node] = true;
+    //     for(int neig : adj.get(node)) {
+    //         if(!vis[neig]) {
+    //             dfs(adj,vis,neig);
+    //         }
+    //     }
+    // }
+
     public void bfs(List<List<Integer>> adj,boolean[] vis,int node) {
         int n = adj.size();
         Queue<Integer>q = new LinkedList<>();
@@ -19,6 +28,7 @@ class Solution {
         boolean[] vis = new boolean[n];
         vis[0] = true;
         bfs(adj,vis,0); // here we put 0(means idx of that node) after that apply bfs
+       // dfs(adj,vis,0);
         for(boolean ele : vis) {
             if(ele==false)return false;
         }
