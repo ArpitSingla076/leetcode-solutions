5fc81a3dadd6495fd966ba1b3fd7eb8df900e6e4
Arpit Singla <157092736+ArpitSingla076@users.noreply.github.com>
2026-08-12 23:18:19 +0530

    Time: 7 ms (99.08%), Space: 48.9 MB (75.6%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,26 +1,24 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b) -> a[0]-b[0]);
-        List<int[]>ans = new ArrayList<>();
-        int currst = intervals[0][0];
-        int currend = intervals[0][1];
-        for(int i=1;i<intervals.length;i++) {
+       // sort by starting time
+       Arrays.sort(intervals,(a,b) -> a[0]-b[0]);
+       List<int[]>ans = new ArrayList<>();
+       int currst = intervals[0][0];
+       int currend = intervals[0][1];
+       for(int i=1;i<intervals.length;i++) {
+        if(intervals[i][0]<=currend) {
             // overlap
-            if(intervals[i][0]<=currend) {
-                currend = Math.max(currend,intervals[i][1]);
-            }
-
-            // no overlap
-            else{
-                ans.add(new int[]{currst,currend});
-
-                currst = intervals[i][0];
-                currend = intervals[i][1];
-            }
+            currend = Math.max(currend,intervals[i][1]);
         }
-        // add last interval
-        ans.add(new int[]{currst,currend});
-        
-        return ans.toArray(new int[ans.size()][]);
+        else{
+            // no overlap
+            ans.add(new int[]{currst,currend});
+            currst = intervals[i][0];
+            currend = intervals[i][1];
+        }
+       }
+       // add last interval
+       ans.add(new int[]{currst,currend});
+       return ans.toArray(new int[ans.size()][]);
     }
 }
